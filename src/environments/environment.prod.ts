030fe1574e50a6e52bf3cd0be694f4c25db4b4b6
mFlaifel <msf-2oo7@hotmail.com>
2021-09-11 20:30:17 +0300

    setup routes, install firebase and bootstrap, create an empty component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD8UgABTsF626LOVtVyMFdmI8XnEzdmE4A',
+    authDomain: 'angular-auth-test-641b9.firebaseapp.com',
+    projectId: 'angular-auth-test-641b9',
+    storageBucket: 'angular-auth-test-641b9.appspot.com',
+    messagingSenderId: '996975892984',
+    appId: '1:996975892984:web:25dae8c57c83f2e6971e0d',
+  },
 };
